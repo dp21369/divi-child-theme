7345dc869e68c79dd2fc9998cc23534b4e45e711
Devendra  Pali <dp21369@gmail.com>
2025-01-15 10:57:33 +0545

    code has been updated on js and functions file

--- functions.php
+++ functions.php
@@ -15,6 +15,6 @@ add_action( 'wp_enqueue_scripts', 'dt_enqueue_styles' );
 
 function custom_enqueue_styles(){
  wp_enqueue_style('custom-style', get_stylesheet_directory_uri() . '/assets/css/custom-style.css', time(), true);
-wp_enqueue_script('custom-custom-script', get_stylesheet_directory_uri() . '/scripts.js', array('jquery'), time());
+wp_enqueue_script('custom-custom-script', get_stylesheet_directory_uri() . '/assets/js/scripts.js', array('jquery'), time());
 }
 add_action('wp_enqueue_scripts', 'custom_enqueue_styles', 999);
